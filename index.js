04092bebb610a0fab1d6f9560a4f533e94966aa1
frankymacster <fs.sunatori@me.com>
2017-05-28 10:19:52 -0400

    Move queue class to separate file
    Related: https://github.com/frankymacster/cytoscape-bfs/issues/1

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-// https://www.khanacademy.org/computing/computer-science/algorithms/breadth-first-search/p/challenge-implement-breadth-first-search
-var Queue = function() {
-  this.items = [];
-};
-Queue.prototype.enqueue = function(obj) {
-  this.items.push(obj);
-};
-Queue.prototype.dequeue = function() {
-  return this.items.shift();
-};
-Queue.prototype.isEmpty = function() {
-  return this.items.length === 0;
-};
-
 var sleep = function (delay) {
   var start = new Date().getTime();
   while (new Date().getTime() < start + delay);
