760d6cf5e7f3c930be946859d2c3c53f60ac1fc7
frankymacster <fs.sunatori@me.com>
2017-05-28 16:24:58 -0400

    encapsulate bfs steps
    Related: https://github.com/frankymacster/cytoscape-bfs/issues/3

--- bfs.js
+++ bfs.js
@@ -16,7 +16,19 @@ var setCurrentNeighbour = function (neighbours, i) {
   return v;
 };
 
-var doBFS = function (graph, source) {
+var visitNode = function (queue, graph, bfsInfo) {
+  var u = setCurrentNode(queue);
+  for (var i = 0; i < graph[u].length; i++) {
+    var v = setCurrentNeighbour(graph[u], i);
+    if (bfsInfo[v].distance === null) {
+      bfsInfo[v].distance = bfsInfo[u].distance + 1;
+      bfsInfo[v].predecessor = u;
+      queue.enqueue(v);
+    }
+  }
+};
+
+var initBfsInfo = function (graph, source) {
   var bfsInfo = [];
 
   for (var i = 0; i < graph.length; i++) {
@@ -26,21 +38,18 @@ var doBFS = function (graph, source) {
     };
   }
 
+  return bfsInfo;
+};
+
+var doBFS = function (graph, source) {
+  var bfsInfo = initBfsInfo(graph);
   bfsInfo[source].distance = 0;
 
   var queue = new Queue();
   queue.enqueue(source);
 
   while (!queue.isEmpty()) {
-    var u = setCurrentNode(queue);
-    for (var i = 0; i < graph[u].length; i++) {
-      var v = setCurrentNeighbour(graph[u], i);
-      if (bfsInfo[v].distance === null) {
-        bfsInfo[v].distance = bfsInfo[u].distance + 1;
-        bfsInfo[v].predecessor = u;
-        queue.enqueue(v);
-      }
-    }
+    visitNode(queue, graph, bfsInfo);
   }
 
   return bfsInfo;
