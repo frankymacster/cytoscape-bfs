8e54e63cc2203ef43f630941ad2e94c8fef5bbec
frankymacster <fs.sunatori@me.com>
2017-05-28 10:23:29 -0400

    Move BFS code to separate file
    Related: https://github.com/frankymacster/cytoscape-bfs/issues/1

--- index.js
+++ index.js
@@ -3,37 +3,6 @@ var sleep = function (delay) {
   while (new Date().getTime() < start + delay);
 };
 
-var doBFS = function (graph, source) {
-  var bfsInfo = [];
-
-  for (var i = 0; i < graph.length; i++) {
-    bfsInfo[i] = {
-      distance: null,
-      predecessor: null,
-    };
-  }
-
-  bfsInfo[source].distance = 0;
-
-  var queue = new Queue();
-  queue.enqueue(source);
-
-  while (!queue.isEmpty()) {
-    var u = setCurrentNode(queue);
-    for (var i = 0; i < graph[u].length; i++) {
-      var v = setCurrentNeighbour(graph[u], i);
-      if (bfsInfo[v].distance === null) {
-        bfsInfo[v].distance = bfsInfo[u].distance + 1;
-        bfsInfo[v].predecessor = u;
-        queue.enqueue(v);
-      }
-      onVisit(v);
-    }
-  }
-
-  return bfsInfo;
-};
-
 var onVisit = function (node) {
   var visitedNode = cy.nodes(node);
   console.log(visitedNode);
